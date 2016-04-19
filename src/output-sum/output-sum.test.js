f5e23855c90ab79370b7859ef1024fb49a5014fc
Jasper Moelker <j.b.moelker@activeids.nl>
2016-04-19 21:18:09 +0200

    test(output-sum): test initial html with no, one and multiple numbers

--- src/output-sum/output-sum.test.js
+++ src/output-sum/output-sum.test.js
@@ -1,15 +1,23 @@
 const riot = require('riot');
 const tape = require('tape');
 
+const output = (sum) => `<output-sum><output>${sum}</output></output-sum>`;
+
 tape('tag without options should output sum of zero', test => {
     test.plan(1);
-    test.equal(riot.render('output-sum', {}), '<output-sum><output>0</output></output-sum>');
+    test.equal(riot.render('output-sum', {}), output(0));
 });
 
 tape('tag with one number should output that number', test => {
-    test.plan(2);
-    test.equal(riot.render('output-sum', { numbers: [1] }), '<output-sum><output>1</output></output-sum>');
-    test.equal(riot.render('output-sum', { numbers: [3.14] }), '<output-sum><output>3.14</output></output-sum>');
+    test.plan(3);
+    test.equal(riot.render('output-sum', { numbers: [1] }), output(1));
+    test.equal(riot.render('output-sum', { numbers: ['2'] }), output(2)); // strings should also work
+    test.equal(riot.render('output-sum', { numbers: [3.14] }), output(3.14)); // floats should also work
 });
 
-
+tape('tag with multiple numbers should output the sum of those number', test => {
+    test.plan(3);
+    test.equal(riot.render('output-sum', { numbers: [1, 2, 3] }), output(6));
+    test.equal(riot.render('output-sum', { numbers: ['1', '2', '3', '4'] }), output(10)); // strings should also work
+    test.equal(riot.render('output-sum', { numbers: [3.14, '4.20', 5] }), output(12.34)); // floats should also work
+});
